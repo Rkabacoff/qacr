cae3dc1880639e0ac69b489c1b198c9501c70c80
Robert Kabacoff <rkabacoff@wesleyan.edu>
2020-12-21 10:07:50 -0500

    update groupdiff

--- R/groupdiff.R
+++ R/groupdiff.R
@@ -5,18 +5,25 @@
 #' One-way analysis (ANOVA or Kruskal-Wallis Test)
 #' with post-hoc comparisons and plots
 #'
-#' @param formula formula of the form \code{Y ~ X} where \code{Y} is the response
-#' variable, and \code{X} is the categorical explanatory variable.
+#' @param formula use the form \code{Y ~ X} where \code{Y} is the response
+#' variable, and \code{X} is the categorical explanatory variable. \code{X}
+#' will be coerced to be a factor.
 #' @param data a data frame.
 #' @param method character. Either \code{"anova"}, or \code{"kw"} (see details).
 #' @param digits Number of significant digits to print.
-#' @param offset space at top of graph for post-hoc comparison letters.
-#' @param horizontal logical. It \code{TRUE}, boxplots are plotted horizontally.
+#' @param horizontal logical. If \code{TRUE}, boxplots are plotted horizontally.
+#' @param posthoc logical. If \code{TRUE}, perform pairwise post-hoc comparisons
+#' (TukeyHSD for ANOVA and Conover Test for Kuskal Wallis)
 #' @import dplyr
 #' @import ggplot2
 #' @importFrom multcompView multcompLetters
 #' @importFrom PMCMRplus kwAllPairsConoverTest
-#' @return a list with 2 components
+#' @return a list with 3 components:
+#' \describe{
+#' \item{\code{result}}{omnibus test}
+#' \item{\code{summarystats}}{summary statistics}
+#' \item{\code{plot}}{ggplot2 graph}
+#' }
 #' @export
 #' @details
 #' The \code{groupdiff} function performs one of two analyses:
@@ -26,7 +33,13 @@
 #' \item{\code{kw}}{A Kruskal Wallis Rank Sum Test, with Conover
 #' Test post-hoc comparisons.}
 #' }
-#' In each case, a boxplot and summary statistics are provided.
+#' In each case, summary statistics and a grouped boxplots are
+#' provided. In the parametric case, the statistics are n, mean, and
+#' standard deviation. In the nonparametric case the statistics are
+#' n, median, and median absolute deviation. If \code{posthoc = TRUE},
+#' the default, pairwise comparisons of superimposed on the boxplots.
+#' Groups that share a letter are not significantly different (p < .05),
+#' controlling for multiple comparisons.
 #' @seealso \link[PMCMRplus]{kwAllPairsConoverTest},
 #' \link[multcompView]{multcompLetters}.
 #' @examples
@@ -34,25 +47,30 @@
 #' groupdiff(hp ~ gear, mtcars)
 #'
 #' # nonparametric analysis
-#' groupdiff(popularity ~ vehicle_style, cardata, method="kw",
-#'           offset=500, horizontal=TRUE)
+#' groupdiff(popularity ~ vehicle_style, cardata,
+#'           method="kw", horizontal=TRUE)
 groupdiff <- function(formula, data,
                       method=c("anova", "kw"),
                       digits=2,
-                      offset = 5,
-                      horizontal=FALSE){
+                      horizontal=FALSE,
+                      posthoc=TRUE){
 
   x <- as.character(formula[[3]])
   y <- as.character(formula[[2]])
 
   data <- na.omit(data[c(x, y)])
 
+  # calculate offset
+  if (posthoc){
+    offset <- (max(data[[y]]) - min(data[[y]]))/10
+  } else {
+    offset <- 0
+  }
+
+
   data[x] <- factor(data[[x]])
   data[x] <- reorder(data[[x]], data[[y]], mean, na.rm=TRUE)
   levels(data[[x]]) <- gsub("-", " ", levels(data[[x]]))
-
-  # plot colors
-  #colors <- brewer.pal(nlevels(data[[x]]), "Set3")
 
 
   method <- match.arg(method)
@@ -78,14 +96,17 @@ groupdiff <- function(formula, data,
       as.data.frame()
 
     # posthoc comparisons
-    tHSD <- TukeyHSD(a, ordered = FALSE, conf.level = 0.95)
-    Tukey.levels <- tHSD[[1]][,4]
-    Tukey.labels <- multcompLetters(Tukey.levels)['Letters']
-    plot.labels <- names(Tukey.labels[['Letters']])
-    plot.levels <- data.frame(plot.labels, Tukey.labels,
-                              stringsAsFactors = FALSE,
-                              row.names=NULL)
-    plot.levels$y <- max(data[y], na.rm=TRUE) + offset
+    if (posthoc){
+      tHSD <- TukeyHSD(a, ordered = FALSE, conf.level = 0.95)
+      Tukey.levels <- tHSD[[1]][,4]
+      Tukey.labels <- multcompLetters(Tukey.levels)['Letters']
+      plot.labels <- names(Tukey.labels[['Letters']])
+      plot.levels <- data.frame(plot.labels, Tukey.labels,
+                                stringsAsFactors = FALSE,
+                                row.names=NULL)
+      plot.levels$y <- max(data[y], na.rm=TRUE) + offset
+    }
+
 
     # plot
     p <- ggplot(data, aes_string(x=x, y=y)) +
@@ -96,20 +117,24 @@ groupdiff <- function(formula, data,
       geom_point(data=mystats,
                  mapping=aes_string(x=x, y="mean"),
                  size=1, shape=3) +
-      geom_text(data = plot.levels,
-                aes(x = plot.labels,
-                    y = y,
-                    label = Letters)) +
       theme_minimal() +
       theme( plot.subtitle = element_text(size = 9),
              plot.caption = element_text(size=8),
              legend.position="none") +
       labs(title = "Group Differences",
            subtitle = paste("ANOVA:", fit.result),
-           caption = "Boxplots that share letters are not significantly different (TukeyHSD p < 0.05).\nCrosses are group means. Dashed line is the mean value for all obs.")
+           caption = "Crosses are group means. Dashed line is the mean value for all obs.")
+    if(posthoc){
+      p <- p +  geom_text(data = plot.levels,
+                          aes(x = plot.labels,
+                              y = y,
+                              label = Letters),
+                              size=3, fontface="italic") +
+        labs(caption = "Boxplots that share letters are not significantly different (TukeyHSD p < 0.05).\nCrosses are group means. Dashed line is the mean value for all obs.")
+    }
+
 
     if(horizontal) p <- p + coord_flip()
-    plot(p)
 
   }
 
@@ -137,23 +162,26 @@ groupdiff <- function(formula, data,
 
 
     # posthoc
-    phtest <- kwAllPairsConoverTest(formula, data)
-    n <- ncol(phtest$p.value)
-    w <- matrix(nrow=n+1, ncol=n+1)
-    nms <- c(dimnames(phtest$p.value)[[2]], dimnames(phtest$p.value)[[1]][n])
-    dimnames(w) <- list(nms, nms)
-    w[2:nrow(w), 1:n] <- phtest$p.value
+    if(posthoc){
+      phtest <- kwAllPairsConoverTest(formula, data)
+      n <- ncol(phtest$p.value)
+      w <- matrix(nrow=n+1, ncol=n+1)
+      nms <- c(dimnames(phtest$p.value)[[2]], dimnames(phtest$p.value)[[1]][n])
+      dimnames(w) <- list(nms, nms)
+      w[2:nrow(w), 1:n] <- phtest$p.value
 
-    makeSymm <- function(m) {
-      m[upper.tri(m)] <- t(m)[upper.tri(m)]
-      return(m)
+      makeSymm <- function(m) {
+        m[upper.tri(m)] <- t(m)[upper.tri(m)]
+        return(m)
+      }
+      w <- makeSymm(w)
+      plot.levels <- data.frame(
+        plot.labels = rownames(w),
+        Letters = as.list(multcompLetters(w))$Letters,
+        y = max(data[y], na.rm=TRUE) + offset
+      )
     }
-    w <- makeSymm(w)
-    plot.levels <- data.frame(
-      plot.labels = rownames(w),
-      Letters = as.list(multcompLetters(w))$Letters,
-      y = max(data[y], na.rm=TRUE) + offset
-    )
+
 
     # plot
     p <- ggplot(data, aes_string(x=x, y=y)) +
@@ -161,23 +189,28 @@ groupdiff <- function(formula, data,
                  linetype="dashed",
                  color="darkgrey") +
       geom_boxplot(aes_string(fill = x)) +
-      geom_text(data = plot.levels,
-                aes(x = plot.labels,
-                    y = y,
-                    label = Letters)) +
       theme_minimal() +
       theme( plot.subtitle = element_text(size = 9),
              plot.caption = element_text(size=8),
              legend.position="none") +
       labs(title = "Group Differences",
            subtitle = paste("Kruskal-Wallis:", fit.result),
-           caption = "Boxplots that share letters are not significantly different (Conover Test p < 0.05).\nDashed line is the median value for all obs.")
+           caption = "Dashed line is the median value for all obs.")
+
+    if(posthoc){
+      p <- p + geom_text(data = plot.levels,
+                aes(x = plot.labels,
+                    y = y,
+                    label = Letters),
+                size=3, fontface="italic") +
+        labs(caption = "Boxplots that share letters are not significantly different (Conover Test p < 0.05).\nDashed line is the median value for all obs.")
+    }
+
     if(horizontal) p <- p + coord_flip()
-    plot(p)
   }
 
   # return results
-  result <- list(result = fit.result, summarystats = mystats)
+  result <- list(result = fit.result, summarystats = mystats, plot=p)
   return(result)
 }
 
